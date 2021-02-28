f4f0f2920d9015475b374b3118829dd4f376f680
suman <suman@pop-os.localdomain>
2021-02-28 14:28:06 +0530

    Downgrade to laravel 5.7

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,8 +13,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::get('/test', function () {
-    return "Test works!!!";
 });
